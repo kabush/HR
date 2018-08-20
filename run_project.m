e86c069053b29c8cf6366705e19bd3b173544258
kabush <kabush@gmail.com>
2018-08-20 11:29:43 -0400

    first commit

---
+++ run_project.m
@@ -0,0 +1,67 @@
+%%========================================
+%%========================================
+%%
+%% Keith Bush, PhD (2018)
+%% Univ. of Arkansas for Medical Sciences
+%% Brain Imaging Research Center (BIRC)
+%%
+%%========================================
+%%========================================
+
+%% ------------------------------------------------------------
+%% Clean up matlab environment
+matlab_reset; % debug
+
+tic
+
+%% ------------------------------------------------------------
+%% Link all source code
+addpath(genpath('./source/'));
+
+%% ------------------------------------------------------------
+%% STEP 1: Initialize the projects directories and parameters.
+init_project;
+
+%% ------------------------------------------------------------
+%% STEP 2: Clear and reconstruct the project data folder
+clean_project;
+
+%% ------------------------------------------------------------
+%% STEP 3: Preprocess raw data (wrangling, filtering, formatting)
+
+%% fMRI data
+preprocess_fmri;
+preprocess_mask;
+% - preprocess_fd;
+
+%% ------------------------------------------------------------
+%% STEP 4: Format Extrinsic Stimuli Design
+format_ex_3dlss; 
+
+%% ------------------------------------------------------------
+%% STEP 5: Calculate Extrinsic (EX) Stimuli Beta-Series
+
+%% Physio betas (these are the targets)
+calc_hrv_ex_beta;  % bpm trajectories | neutral filtering
+calc_hrv_ex_bpm;  % bpm targets for mvpa
+
+%% fMRI betas (these are the features)
+calc_fmri_ex_beta;
+
+%% ------------------------------------------------------------
+%% STEP 7: Conduct MVPA for Secondary Measures
+mvpa_fmri_ex_gm_rgr_hrv_inter_thresh % inter-subj Gray-matter
+                                     % regression.
+mvpa_fmri_ex_gm_rgr_hrv_inter_all    % inter-subj Gray-matter
+                                     % regression.
+
+%% ------------------------------------------------------------ 
+%% STEP 12: Secondary Analysis of EX physiology (Neuropsychologia Paper)
+analyze_ex_hrv_mvpa % working DRAFT
+
+%% ------------------------------------------------------------ 
+%% STEP 13: Hyperplane analysis
+haufe_ex_gm_hrv_mvpa_all_permute  % (this runs for weeks)
+haufe_ex_gm_hrv_mvpa_thresh_permute % (this runs for days)
+
+toc
