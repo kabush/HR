776ab3060fb4fc65aef078f81d31203ba86aae84
Keith Bush <kabush@gmail.com>
2019-04-16 10:16:16 -0500

    run_project tidy-up prior to manuscript submission

--- run_project.m
+++ run_project.m
@@ -22,61 +22,61 @@ addpath(genpath('./source/'));
 %% STEP 1: Initialize the projects directories and parameters.
 init_project;
 
-% %% ------------------------------------------------------------
-% %% STEP 2: Clear and reconstruct the project data folder
-% clean_project;
-% 
-% %% ------------------------------------------------------------
-% %% STEP 3: Preprocess raw data (wrangling, filtering, formatting)
-% 
-% % fMRI data
-% preprocess_fmri;
-% preprocess_mask;
-% 
-% % HRV data (convert to csv for use in Kubios software)
-% preprocess_hrv_csv;
-% 
-% %% ******************************
-% %% ******************************
-% %% THERE IS A MANUAL KUBIOS 
-% %% PROCESSING STEP HERE
-% %% ******************************
-% %% ******************************
-% 
-% % convert kubios *.mat format to *.csv format for python code
-% preprocess_hrv_kubios_reformat; 
-% 
-% %% ------------------------------------------------------------
-% %% STEP 4: Format Extrinsic Stimuli Design
-% format_ex_3dlss; 
-% 
-% %% ------------------------------------------------------------
-% %% STEP 5: Calculate Extrinsic (EX) Stimuli Beta-Series
-% 
-% % Physio betas (these are the targets)
-% calc_hrv_ex_beta; % bpm trajectories | neutral filtering
-% calc_hrv_ex_bpm;  % bpm targets for mvpa
-% 
-% %% fMRI betas (these are the features)
-% calc_fmri_ex_beta;
-% 
-% %% ------------------------------------------------------------
-% %% STEP 7: Conduct MVPA for Secondary Measures
-% mvpa_fmri_ex_gm_rgr_hrv_inter_thresh; % inter-subj Gray-matter
-%                                       % regression.
-% mvpa_fmri_ex_gm_rgr_hrv_inter_all;    % inter-subj Gray-matter
-%                                       % regression.
+%% ------------------------------------------------------------
+%% STEP 2: Clear and reconstruct the project data folder
+clean_project;
 
-% %% ------------------------------------------------------------
-% %% STEP 8: Compare MVPA hyperplanes (V vs HR)
+%% ------------------------------------------------------------
+%% STEP 3: Preprocess raw data (wrangling, filtering, formatting)
+
+% fMRI data
+preprocess_fmri;
+preprocess_mask;
+
+% HRV data (convert to csv for use in Kubios software)
+preprocess_hrv_csv;
+
+%% ******************************
+%% ******************************
+%% THERE IS A MANUAL KUBIOS 
+%% PROCESSING STEP HERE
+%% ******************************
+%% ******************************
+
+% convert kubios *.mat format to *.csv format for python code
+preprocess_hrv_kubios_reformat; 
+
+%% ------------------------------------------------------------
+%% STEP 4: Format Extrinsic Stimuli Design
+format_ex_3dlss; 
+
+%% ------------------------------------------------------------
+%% STEP 5: Calculate Extrinsic (EX) Stimuli Beta-Series
+
+% Physio betas (these are the targets)
+calc_hrv_ex_beta; % bpm trajectories | neutral filtering
+calc_hrv_ex_bpm;  % bpm targets for mvpa
+
+%% fMRI betas (these are the features)
+calc_fmri_ex_beta;
+
+%% ------------------------------------------------------------
+%% STEP 7: Conduct MVPA for Secondary Measures
+mvpa_fmri_ex_gm_rgr_hrv_inter_thresh; % inter-subj Gray-matter
+                                      % regression.
+mvpa_fmri_ex_gm_rgr_hrv_inter_all;    % inter-subj Gray-matter
+                                      % regression.
+
+%% ------------------------------------------------------------
+%% STEP 8: Compare MVPA hyperplanes (V vs HR)
 analyze_ex_v_vs_hrv_mvpa;
 
-% %% ------------------------------------------------------------ 
-% %% STEP 9: Secondary Analysis of EX physiology (Neuropsychologia Paper)
-% analyze_ex_hrv_mvpa;
-% 
-% %% ------------------------------------------------------------ 
-% %% STEP 10: Hyperplane analysis
-% haufe_ex_gm_hrv_mvpa_all_permute;
+%% ------------------------------------------------------------ 
+%% STEP 9: Secondary Analysis of EX physiology (Neuropsychologia Paper)
+analyze_ex_hrv_mvpa;
+
+%% ------------------------------------------------------------ 
+%% STEP 10: Hyperplane analysis
+haufe_ex_gm_hrv_mvpa_all_permute;
 
 toc
